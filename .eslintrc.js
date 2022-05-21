91d6c807e531ba89e7def96460de8b41c0b75fc6
kityandhero <kityandhero@126.com>
2022-05-21 16:21:41 +0800

    style: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  extends: [require.resolve('@umijs/fabric/dist/eslint'), 'plugin:react/jsx-runtime'],
   globals: {
     page: true,
     REACT_APP_ENV: true,
